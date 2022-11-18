485cdf6da71b46b644ce266df70d4f3ede367ec8
Arabinda <arabinda.mcs@gmail.com>
2022-11-18 19:35:33 +0100

    version 2

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const PORT = process.env.PORT || 9000
 http
   .createServer((req, res) => {
     console.log('New connection')
-    res.end('Hello Cloud Run - version 1')
+    res.end('Hello Cloud Run - version 2')
   })
   .listen(PORT, () => console.log('Listening on', PORT))
